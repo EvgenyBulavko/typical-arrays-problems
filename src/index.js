da1b87eb616635324de6508ca1b01df4eb5c44f9
Bulavko Evgeniy <z-bulavko@mail.ru>
2020-03-14 18:56:29 +0300

    feat: add index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+
+  if (Array.isArray(array) == false) { return 0; }
+  let s = array[0];
+  if (!array.length) return 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < s) s = array[i];
+  }
+
+  return s;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+
+
+  if (Array.isArray(array) == false) { return 0; }
+  let s = array[0];
+  if (!array.length) return 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > s) s = array[i];
+  }
+  return s;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let s = 0;
+
+  if (Array.isArray(array) == false) { return 0; }
+  if (!array.length) return 0;
+
+  for (let i = 0; i < array.length; i++) {
+    s = s + array[i];
+  }
+  return s / array.length;
 }
